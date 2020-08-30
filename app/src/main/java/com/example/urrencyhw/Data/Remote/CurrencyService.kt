90920b7b5b7b5041e8e34c17645c005c57a640b8
Aiperi <aip.amankulova@gmail.com>
2020-08-30 21:33:11 +0600

    Committing new files

---
+++ app/src/main/java/com/example/urrencyhw/Data/Remote/CurrencyService.kt
@@ -0,0 +1,12 @@
+package com.example.urrencyhw.Data.Remote
+
+
+import com.example.urrencyhw.Data.Model.DataClass
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CurrencyService {
+    @GET("latest")
+    fun getCurrencies(@Query("access_key") key: String): Call<DataClass>
+}
